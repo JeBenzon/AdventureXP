a1c5afa0d46daec34d977789e2db97532ab2a23f
Martin Holmqvist <54819258+HolmQ84@users.noreply.github.com>
2020-02-24 15:15:46 +0100

    Add files via upload

---
+++ AdventureXP/src/main/java/adventurexp/demo/AdventureXpApplication.java
@@ -0,0 +1,13 @@
+package adventurexp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureXpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureXpApplication.class, args);
+    }
+
+}
---
+++ AdventureXP/src/test/java/adventurexp/demo/AdventureXpApplicationTests.java
@@ -0,0 +1,13 @@
+package adventurexp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureXpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
